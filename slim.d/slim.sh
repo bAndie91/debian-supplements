17ff9df76ed824c005748f276a297e5ff672a0db
Andreas Hrubak <ad132454@uucp.hu>
2023-12-24 16:07:57 +0100

    x

--- slim.d/slim.sh
+++ slim.d/slim.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-display=$1
-DISPLAY=:$display
+displaynum=$1
+DISPLAY=:$displaynum
 export DISPLAY
-exec /usr/local/bin/slim -n -c /etc/slim.d/$display.conf
+SLIM_CONFIG=/etc/slim.d/$displaynum.conf
+export SLIM_CONFIG
+exec /usr/local/bin/slim -n -c "$SLIM_CONFIG"
