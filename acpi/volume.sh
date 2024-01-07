8de26d92151180b00054921d48a6be494420d97d
Andreas Hrubak <ad132454@uucp.hu>
2024-01-07 22:28:07 +0100

    headphones are muted when one is plugged in

--- acpi/volume.sh
+++ acpi/volume.sh
@@ -11,6 +11,8 @@ if [ $direction = up ]
 then
 	amixer sset Master on
 	amixer sset Speaker on   # thanks xflow7 for the tip
+	amixer sset Headphone on
+	amixer sset Headphone,1 on
 fi
 
 exec amixer sset Master 5%$sign
