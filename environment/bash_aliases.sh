32a1da2632d54c6c7967f7eeb1dfcd23a490ae8b
Andreas Hrubak <ad132454@uucp.hu>
2024-06-01 13:09:10 +0200

    bash aliases

---
+++ environment/bash_aliases.sh
@@ -0,0 +1,15 @@
+if [ "x${BASH_VERSION-}" != x -a "x${PS1-}" != x ]
+then
+	if ! alias ll 2>/dev/null >&2
+	then
+		alias ll='ls -lA'
+	fi
+	if ! alias l 2>/dev/null >&2
+	then
+		alias l='ls -CFA'
+	fi
+	if ! alias .. 2>/dev/null >&2
+	then
+		alias ..='cd ..'
+	fi
+fi
